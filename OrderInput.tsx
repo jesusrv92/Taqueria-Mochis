dd2e92aa372d6b653a38d959664311ad01acf9d6
jesusrv92 <53059134+jesusrv92@users.noreply.github.com>
2023-02-07 19:05:44 -0700

    fix: clear temporders on reset

--- OrderInput.tsx
+++ OrderInput.tsx
@@ -49,7 +49,10 @@ export const OrderInput = () => {
       <div className="w-1/3 sticky top-0 mr-1">
         <button
           className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-          onClick={() => setOrder([])}
+          onClick={() => {
+            tempOrders = {};
+            setOrder([]);
+          }}
         >
           Reset
         </button>
